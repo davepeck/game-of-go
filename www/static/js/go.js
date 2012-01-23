98150c9d4471e09fd25e552495ad3a0995106ec3
Duncan Exon Smith <duncanphilipnorman@gmail.com>
2012-01-23 11:56:37 -0500

    Fixing capture message
    When komi was added, the capture message was broken.

--- www/static/js/go.js
+++ www/static/js/go.js
@@ -1824,7 +1824,8 @@ var GameController = Class.create({
     {
         if (this.state.are_stones_captured())
         {
-            $("capture_message").removeClassName("hide");
+            $("capture_submessage_1").removeClassName("hide");
+            $("capture_submessage_2").removeClassName("hide");
         }
     },
 
